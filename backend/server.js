1f46ec8515d34f28aa058cce892f6c2aeaff3577
JoungYujin <jyjin0205@postech.ac.kr>
2025-03-07 14:57:10 +0900

    error fix

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 
+const app = express();
+app.use(cors()); // CORS 미들웨어 추가
+app.use(express.json()); // JSON 요청을 파싱
+
 const userRouter = express.Router();
 
-const app = express();
-app.use(express.json());
+userRouter.get('/', (req, res) => {
+    res.send('서버 실행 중!');
+});
 
-app.use('/api/users',userRouter);
-
-userRouter.post('/sayHello', function(req,res) {
+userRouter.post('/sayHello', (req, res) => {
     const responseBody = {
         version: "2.0",
         template: {
-          outputs: [
-            {
-              simpleText: {
-                text: "hello I'm Ryan"
-              }
-            }
-          ]
+            outputs: [
+                {
+                    simpleText: {
+                        text: "나나리즘"
+                    }
+                }
+            ]
         }
-      };
+    };
     
-      res.status(200).send(responseBody);
+    res.status(200).send(responseBody);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.use('/api/users', userRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중...`);
+});
